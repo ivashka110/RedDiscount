d70731b9fdc7d1c92a94d4b7c1b16b9ee3c12946
Ivan Radygin <ivan.radygin@yandex.ru>
2016-04-13 23:27:11 +0400

    new files

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('homeTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('locationTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('productTabDefaultPageCtrl', function($scope) {
+
+})
+       
